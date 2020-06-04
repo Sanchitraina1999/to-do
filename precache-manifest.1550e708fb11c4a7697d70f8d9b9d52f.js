e8cd266c909ace69e634ac9ef6d12cb8426a3297
Sanchitraina1999 <sanchitraina1999@gmail.com>
2020-06-04 14:51:21 +0530

    Updates

---
+++ precache-manifest.1550e708fb11c4a7697d70f8d9b9d52f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e47955b939ebc48568eda5a340fe73b1",
+    "url": "/to-do/index.html"
+  },
+  {
+    "revision": "dea11facdc9e4238e2fe",
+    "url": "/to-do/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c4236174b61a25951732",
+    "url": "/to-do/static/js/2.0b1e0712.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/to-do/static/js/2.0b1e0712.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dea11facdc9e4238e2fe",
+    "url": "/to-do/static/js/main.6a318180.chunk.js"
+  },
+  {
+    "revision": "3c5dbf546597a235a261",
+    "url": "/to-do/static/js/runtime-main.27ca5ea1.js"
+  }
+]);
